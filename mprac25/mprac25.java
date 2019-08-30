8678ebc993fe30239e6622cd4848355c2fa195e3
Madhav tripathi <madhav.37.tripathi@gmail.com>
2019-08-30 14:05:15 +0530

    added Part 4

---
+++ mprac25/mprac25.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class mprac25 {
+    static void functionWithException() throws Exception{
+        System.out.println("fun with exception");
+        throw new IllegalAccessError("illegal access Exception");
+    }
+
+    public static void main(String[] args) {
+
+    try {
+        functionWithException();
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+
+    System.out.println("\nNAME:MADHAV\nID:18DCS129\n");
+    }
+}
